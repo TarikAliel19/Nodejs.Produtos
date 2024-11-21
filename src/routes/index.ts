3485b584af172321e04ffe154150c1f5285b1c9d
TARIK ALIEL FERREIRA ARAUJO SANTANA <tarik.afasantana@senacsp.edu.br>
2024-11-21 17:16:08 -0300

    Editar e atualizar Produtos

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import {Router} from 'express'
+import * as produtoController from '../controllers/produtoController'
+
+const router = Router()
+
+router.get('/',(req,res) =>{
+    res.send('teste')
+})
+
+router.get('/produtos',produtoController.index)
+router.get('/cadastrar', produtoController.visualizarCadastro)
+router.post('/cadastrar', produtoController.cadastroProduto)
+router.get('/editar/:id', produtoController.editaProduto)
+router.post('/editar/:id', produtoController.atualizaProduto)
+
+export default router
